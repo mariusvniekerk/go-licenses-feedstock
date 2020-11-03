719850b63c7b54a336f86571928d0e1be321ad7c
Marcel Bargull <marcel.bargull@udo.edu>
2020-11-03 16:16:36 +0100

    Add $PREFIX/share/go-licenses/licenses.db

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,4 +7,9 @@ export GOBIN=$GOBIN
 
 go get -v github.com/google/go-licenses
 
-$GOBIN/go-licenses save $SRC_DIR --save_path="$RECIPE_DIR/thirdparty_licenses/"
+mkdir -p "${PREFIX}/share/go-licenses"
+cp \
+  "${GOPATH:-"$( go env GOPATH )"}"/pkg/mod/github.com/google/licenseclassifier*/licenses/licenses.db \
+  "${PREFIX}/share/go-licenses/"
+
+go-licenses save $SRC_DIR --save_path="$RECIPE_DIR/thirdparty_licenses/"
