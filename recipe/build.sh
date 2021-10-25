94c9f12f3c390cac5f6950d6b9957a16146bb37b
Marcel Bargull <marcel.bargull@udo.edu>
2021-10-25 17:56:09 +0200

    Cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -ex
 
-
+export GOBIN
 GOBIN=$(go env GOBIN)
-export GOBIN=$GOBIN
 
 go install -v github.com/google/go-licenses
 
 go-licenses save . --save_path=./license-files
 rm -r ./license-files/github.com/google/licenseclassifier/licenses
+
+# Make GOPATH directories writeable so conda-build can clean everything up.
+find "$( go env GOPATH )" -type d -exec chmod +w {} \;
