10d72ea04d585c3f37e8b9c9c938ba6bf5f09ee2
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-07-18 01:08:28 -0500

    Add go-licenses for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,20 @@
 export GOBIN
 GOBIN=$(go env GOBIN)
 
+if [[ "${target_platform}" == "osx-64" ]]; then
+  export GOOS=darwin
+  export GOARCH=amd64
+elif [[ "${target_platform}" == "osx-arm64" ]]; then
+  export GOOS=darwin
+  export GOARCH=arm64
+elif [[ "${target_platform}" == "linux-aarch64" ]]; then
+  export GOOS=linux
+  export GOARCH=arm64
+elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
+  export GOOS=linux
+  export GOARCH=ppc64le
+fi
+
 go install -v github.com/google/go-licenses
 
 go-licenses save . --save_path=./license-files
